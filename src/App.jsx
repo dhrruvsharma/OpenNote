5d5134a8046f432bae0a95e5dac61ed95bd6b476
dhrruvsharma <dsd71213@gmail.com>
2024-11-27 00:56:47 +0530

    Fixing

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,12 @@ import Dash from "./Components/Dash"
 import "./App.css"
 import Dashboard from "./Components/Dashboard/Dashboard"
 import Create from "./Components/Create/Create"
-import Test from "./Test"
 
 const App = () => {
   return(
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Test />}>
+        <Route path="/" element={<Dashboard />}>
           <Route path="create" element={<Create />} />
           <Route path="" element={<Dash />} />
         </Route>
